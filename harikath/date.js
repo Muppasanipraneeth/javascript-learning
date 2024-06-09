a0362d49c0d6f16d53c56117c584f1cc12afc0f0
praneeth <muppasanipraneeth19@gmail.com>
2024-06-09 17:42:15 +0530

    learned about date

---
+++ harikath/date.js
@@ -0,0 +1,14 @@
+// console.log(new Date(8.64e15).toString());
+// function printig(){
+//     let mydate=new Date();
+// console.log(mydate);
+// return ;
+// }
+// setInterval(printig, 1000);
+let mydate=new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toTimeString( ));
